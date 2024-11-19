38ffd73501f2100c1ed475fa19408568c8eb22f4
suhail <suhailanzar00@gmail.com>
2024-11-19 15:23:38 +0530

    changed the mongo uri to atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/cms', {
+    MongooseModule.forRoot('mongodb+srv://suhailanz01:KKbNkmSmgJYhWBoW@cluster0.41triad.mongodb.net/?retryWrites=true&w=majority/cms', {
     }),
     
     AuthorizationModule,
