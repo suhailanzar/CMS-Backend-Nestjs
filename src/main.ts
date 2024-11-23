6ee9a203649f230cf5d95ea94ddf2d81ac607d1a
suhail <suhailanzar00@gmail.com>
2024-11-23 10:10:40 +0530

    updated the url added a slash

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: 'http://localhost:4200', 
-    origin: 'https://cms-beta-self.vercel.app', 
+    origin: 'https://cms-beta-self.vercel.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }
